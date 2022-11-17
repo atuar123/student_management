4ed2bb25985685eddcd9e40154b36cbd765af203
atuar <atuar.cse@gmail.com>
2022-11-17 17:40:05 +0600

    swagger enable

--- src/main/java/com/ctechbd/student_management/controller/StudentController.java
+++ src/main/java/com/ctechbd/student_management/controller/StudentController.java
@@ -17,29 +17,29 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @PostMapping("save-student")
+    @PostMapping("/save-student")
     public boolean saveStudent(@RequestBody Student student) {
         return studentService.saveStudent(student);
 
     }
 
-    @GetMapping("students-list")
+    @GetMapping("/students-list")
     public List<Student> allstudents() {
         return studentService.getStudents();
     }
 
-    @DeleteMapping("delete-student/{studentId}")
+    @DeleteMapping("/delete-student/{studentId}")
     public boolean deleteStudent(@PathVariable("studentId") int student_id,Student student) {
         return studentService.deleteStudent(student);
     }
 
-    @GetMapping("student/{studentId}")
+    @GetMapping("/student/{studentId}")
     public Student getStudentByID(@PathVariable("studentId") int student_id,Student student) {
         return studentService.getStudentByID(student_id);
     }
 
-    @PutMapping("update-student/{studentId}")
-    public boolean updateStudent(@RequestBody Student student) {
+    @PutMapping("/update-student/{studentId}")
+    public boolean updateStudent(@RequestBody Student student, @PathVariable String studentId) {
         return studentService.updateStudent(student);
     }
 }
